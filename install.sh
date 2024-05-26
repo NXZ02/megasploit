6e3da9f2ddeb534370a1d57e6e76b437c927cb6d
NXZ02 <109139829+NXZ02@users.noreply.github.com>
2024-05-26 12:08:20 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,14 +14,16 @@ read -p "choose : " tk
 
 if [ $tk == 1 ];
 then
+ 	sudo apt install python3-pip -y 
 	pip install pygame --break-system-packages
 	pip install tk --break-system-packages
 	pip install customtkinter --break-system-packages
 	pip install pillow --break-system-packages
 	sudo apt install python3-dev -y 
-	sudo apt install python3-pil python3-pil.imagetk
+	sudo apt install python3-pil python3-pil.imagetk -y 
 elif [ $tk == 2 ]; 
 	then
+ 		sudo pacman -S python-pip --noconfirm
 		sudo pacman -S tk --noconfirm
 		pip install tk --break-system-packages
 		pip install customtkinter --break-system-packages
